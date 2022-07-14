f53eeb318ec3d1ca0dd210c1e6d299669a1a358c
nguyen chi. thanh <nguyenchithanh@kng-vn.com>
2022-07-14 17:35:41 +0700

    Drupal 9 Pagination- search action-loadTag ::new Reup

---
+++ modules/custom/post_module/src/Plugin/Block/TagsBlock.php
@@ -0,0 +1,52 @@
+<?php
+
+namespace Drupal\post_module\Plugin\Block;
+
+use Drupal\Core\Block\BlockBase;
+
+/**
+ * Provides a 'Hello' Block.
+ *
+ * @Block(
+ *   id = "tags_block",
+ *   admin_label = @Translation("Tag"),
+ *   category = @Translation("Tab Block"),
+ * )
+ */
+class TagsBlock extends BlockBase {
+
+  /**
+   * {@inheritdoc}
+   */
+  public function build() {
+
+    $lang_code = \Drupal::languageManager()->getCurrentLanguage()->getId();
+//
+    $query = \Drupal::entityQuery('taxonomy_term');
+    $query_or = $query->orConditionGroup()
+      ->condition('vid', 'tags')
+      ->condition('vid', 'log');
+    $query->condition($query_or);
+    $tids = $query->execute();
+    //var_dump($tids);
+    $tags = \Drupal\taxonomy\Entity\Term::loadMultiple($tids);
+
+    foreach ($tags as &$tag) {
+      if ($tag->hasTranslation($lang_code)) {
+        $tag = \Drupal::service('entity.repository')->getTranslationFromContext($tag, $lang_code);
+      }
+      $tag->url=$tag->toUrl($rel = 'canonical',
+        $options = [
+          'language' => \Drupal::languageManager()->getLanguage($tag->langcode),
+        ]
+      )->toString();
+    }
+
+    return [
+      '#markup' => $this->t('Tags'),
+      '#theme' => 'tags_block',
+      '#tags' => $tags
+    ];
+  }
+
+}
